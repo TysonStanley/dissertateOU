44cba4fc3fbce5d85791a118197f8f75966fb2e3
TysonStanley <t.barrett@aggiemail.usu.edu>
2020-06-23 20:00:23 -0600

    Initial commit

---
+++ R/dissertateOU.R
@@ -0,0 +1,71 @@
+#' dissertateOU format.
+#'
+#' Format for creating dissertations at Utah State University. It has been approved by the graduate school.
+#'
+#' @inheritParams rmarkdown::pdf_document
+#' @param ... Additional arguments to \code{rmarkdown::pdf_document}
+#'
+#' @return R Markdown output format to pass to
+#'   \code{\link[rmarkdown:render]{render}}
+#'
+#' @details Possible arguments for the YAML header are:
+#' \itemize{
+#'   \item \code{title} title of the dissertation
+#'   \item \code{author} list of authors, containing \code{name} and \code{num}
+#'   \item \code{year} year of defense
+#'   \item \code{degree} degree earned (e.g. "Doctor of Philosophy")
+#'   \item \code{field} the field the degree is awarded in
+#'   \item \code{academicunit} the academic unit awarding the degree
+#'   \item \code{thesis} whether this is a thesis or dissertation (true means thesis)
+#'   \item \code{chair} name of the defense committee chairperson
+#'   \item \code{committee2} name of second committee member
+#'   \item \code{committee3} name of third committee member
+#'   \item \code{committee4} name of fourth committee member
+#'   \item \code{committee5} name of fifth committee member (if left blank, committee4 will be ignored and the cover page will be formatted as a masters thesis)
+#'   \item \code{bibliography} BibTeX \code{.bib} file name
+#'   \item \code{csl} the .csl citation style file name
+#'   \item \code{header-includes}: custom additions to the header, before the \code{\\begin\{document\}} statement
+#'   \item \code{include-after}: for including additional LaTeX code before the \code{\\end\{document\}} statement
+#'   }
+#'
+#' @examples
+#'
+#' \dontrun{
+#' library(rmarkdown)
+#' draft("MyArticle.Rmd", template = "dissertateOU", package = "dissertateOU")
+#' }
+#'
+#' @import rticles
+#' @import yaml
+#'
+#' @export
+dissertateOU <- function(..., highlight = NULL, citation_package = "none") {
+
+  # Find template in dissertateOU
+  template_file <-
+    system.file("rmarkdown", "templates", "dissertateOU",
+                file.path("resources", "template.tex"),
+                package = "dissertateOU")
+
+  # Check template file exists
+  .check_template(template_file)
+
+  # Render the pdf_document with parameters
+  pdf <-
+    rmarkdown::pdf_document(
+    ...,
+    template = template_file,
+    highlight = highlight,
+    citation_package = citation_package
+    )
+
+  # Documentation of inherits
+  pdf$inherits <- "pdf_document"
+
+  # Check pre-rendered output
+  .check_pdf(pdf)
+
+  # Return pre-rendered output
+  pdf
+}
+
